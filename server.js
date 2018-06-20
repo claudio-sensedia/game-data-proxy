7ed6fb1eb3345986aa03bb3cc4b14384e73b8688
Claudio E. de Oliveira <claudio.oliveira@sensedia.com>
2018-06-20 16:33:11 -0300

    jaeger headers

--- server.js
+++ server.js
@@ -11,22 +11,43 @@ const SERVICE_VERSION = process.env.VERSION || 'v1';
 
 const app = express();
 app.get('/matches', (req, res) => {
-  const url = MATCHES_SERVICES;
-  console.log('URL:', url);
-  request(url, function (error, response, body) {
-    console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('body:', body);
-    const content = JSON.parse(body);
-    content.version = SERVICE_VERSION;
-    res.json(content);
-  });
+    const url = MATCHES_SERVICES;
+    console.log('URL:', url);
+    let tracingHeaders = headers(req);
+    let options = {
+        url: url,
+        headers: tracingHeaders
+    };
+    request(options, function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+        const content = JSON.parse(body);
+        content.version = SERVICE_VERSION;
+        res.json(content);
+    });
 });
 
 app.get('/healthcheck', (req, res) => {
-  console.log(`Game Data Proxy Running on http://${HOST}:${PORT}`);
-  res.json({status: "OK", version : SERVICE_VERSION});
+    console.log(`Game Data Proxy Running on http://${HOST}:${PORT}`);
+    res.json({status: "OK", version: SERVICE_VERSION});
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+function headers(req) {
+    let incomingHeaders = ['x-request-id',
+        'x-b3-traceid',
+        'x-b3-spanid',
+        'x-b3-parentspanid',
+        'x-b3-sampled',
+        'x-b3-flags',
+        'x-ot-span-context'
+    ];
+    let headers = {};
+    for (let header of incomingHeaders) {
+        headers[header] = req.get(header);
+    }
+    return headers;
+}
